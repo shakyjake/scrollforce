bad851c133670b2f3913c977455e65cc07885ae6
shakyjake <6040147+shakyjake@users.noreply.github.com>
2020-11-12 13:49:51 +0000

    Add files via upload

---
+++ scrollforce.js
@@ -0,0 +1,47 @@
+(() => {
+    
+    const Body = document.body;
+    const HTML = document.body.parentNode;
+    
+    const MutationConfig = {
+        attributes: true,
+        childList: false,
+        subtree: false
+    };
+    
+    const OriginalOverflowBody = window.getComputedStyle(Body).getPropertyValue('overflow-y');
+    const OriginalOverflowHTML = window.getComputedStyle(HTML).getPropertyValue('overflow-y');
+    
+    const MutationCallback = function(Mutations){
+        Mutations.forEach((Mutation) => {
+            if(Mutation.type === 'attributes'){
+                if(Mutation.attributeName  === 'class' || Mutation.attributeName  === 'style'){
+                    
+                    const Target = Mutation.target;
+                    
+                    if(Target.nodeName.toLowerCase() === 'body'){
+                        const OverflowBody = window.getComputedStyle(Body).getPropertyValue('overflow-y');
+                        if(OverflowBody !== OriginalOverflowBody){
+                            Body.style.overflowY = OriginalOverflowBody;
+                        }
+                    }
+                    
+                    if(Target.nodeName.toLowerCase() === 'html'){
+                        const OverflowHTML = window.getComputedStyle(HTML).getPropertyValue('overflow-y');
+                        if(OverflowHTML !== OriginalOverflowHTML){
+                            HTML.style.overflowY = OriginalOverflowHTML;
+                        }
+                    }
+                    
+                }
+            }
+        });
+    }
+    
+    const Observer = new MutationObserver(MutationCallback);
+
+    Observer.observe(document.body, MutationConfig);
+    Observer.observe(document.body.parentNode, MutationConfig);
+
+
+})();
