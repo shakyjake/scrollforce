2441907caa96db1e6a39176ef5d7e1b1ffd7bff5
shakyjake <6040147+shakyjake@users.noreply.github.com>
2025-11-21 16:37:35 +0000

    V1.2.6 - rewrote the lot, added a Facebook-specific rule and added bug fixes for non-html documents

--- scrollforce.js
+++ scrollforce.js
@@ -1,35 +1,83 @@
+/**
+ * Fix for websites (facebook) that forcibly disable scrolling by setting the root container to position: fixed !important
+ */
+function fix_fb(){
+	const root = document.querySelector('div');
+	if(root){
+		root.style.position = 'relative !important';
+	}
+}
+
+/**
+ * Returns true if a given string ends with another string
+ * @param {string} haystack - the string to search in
+ * @param {string} needle - the string to search for
+ * @returns {boolean}
+ */
+function ends_with(haystack, needle){
+	return haystack.substring(haystack.length - needle.length) === needle;
+}
+
 (() => {
+
+    if(typeof(window) === 'undefined'){/* No window object - must be running in a non-browser environment */
+        return;
+    }
+
+    if(typeof(document) === 'undefined'){/* No document object - mustn't be running in a DOMish environment */
+        return;
+    }
+
+    if(typeof(document.body) === 'undefined'){/* No body - mustn't be an html document */
+        return;
+    }
+
+    if(!document.body){/* No body - mustn't be an html document (document.body is null on svg files) */
+        return;
+    }
+	
+	if('location' in window){
+		if('hostname' in window.location){
+			if(ends_with(window.location.hostname, 'facebook.com')){
+				/*
+					Really didn't want to access window.location but Facebook breaks 
+					scrolling by adding "position: fixed !important" to the root div
+				*/
+				window.addEventListener('load', fix_facebook);
+			}
+		}
+	}
     
-    const Body = document.body;
-    const HTML = document.body.parentNode;
+    const body_node = document.body;
+    const html_node = document.body.parentNode;
     
-    const MutationConfig = {
+    const observer_config = {
         attributes: true,
         childList: false,
         subtree: false
     };
     
-    const OriginalOverflowBody = window.getComputedStyle(Body).getPropertyValue('overflow-y');
-    const OriginalOverflowHTML = window.getComputedStyle(HTML).getPropertyValue('overflow-y');
+    const init_overflow_body = window.getComputedStyle(body_node).getPropertyValue('overflow-y');
+    const init_overflow_html = window.getComputedStyle(html_node).getPropertyValue('overflow-y');
     
-    const MutationCallback = function(Mutations){
-        Mutations.forEach((Mutation) => {
-            if(Mutation.type === 'attributes'){
-                if(Mutation.attributeName  === 'class' || Mutation.attributeName  === 'style'){
+    const observer_callback = function(mutations){
+        mutations.forEach((mutation) => {
+            if(mutation.type === 'attributes'){
+                if(mutation.attributeName  === 'class' || mutation.attributeName  === 'style'){
                     
-                    const Target = Mutation.target;
+                    const mutation_target = mutation.target;
                     
-                    if(Target.nodeName.toLowerCase() === 'body'){
-                        const OverflowBody = window.getComputedStyle(Body).getPropertyValue('overflow-y');
-                        if(OverflowBody !== OriginalOverflowBody){
-                            Body.style.overflowY = OriginalOverflowBody;
+                    if(mutation_target.nodeName.toLowerCase() === 'body'){
+                        const current_overflow = window.getComputedStyle(body_node).getPropertyValue('overflow-y');
+                        if(current_overflow !== init_overflow_body){
+                            body_node.style.overflowY = init_overflow_body;
                         }
                     }
                     
-                    if(Target.nodeName.toLowerCase() === 'html'){
-                        const OverflowHTML = window.getComputedStyle(HTML).getPropertyValue('overflow-y');
-                        if(OverflowHTML !== OriginalOverflowHTML){
-                            HTML.style.overflowY = OriginalOverflowHTML;
+                    if(mutation_target.nodeName.toLowerCase() === 'html'){
+                        const current_overflow = window.getComputedStyle(html_node).getPropertyValue('overflow-y');
+                        if(current_overflow !== init_overflow_html){
+                            html_node.style.overflowY = init_overflow_html;
                         }
                     }
                     
@@ -38,14 +86,14 @@
         });
     }
     
-    const Observer = new MutationObserver(MutationCallback);
+    const observer = new MutationObserver(observer_callback);
 
-    if(OriginalOverflowBody !== 'hidden'){
-        Observer.observe(Body, MutationConfig);
+    if(init_overflow_body !== 'hidden'){
+        observer.observe(body_node, observer_config);
     }
 
-    if(OriginalOverflowHTML !== 'hidden'){
-        Observer.observe(HTML, MutationConfig);
+    if(init_overflow_html !== 'hidden'){
+        observer.observe(html_node, observer_config);
     }
 
 
